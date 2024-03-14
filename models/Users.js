cfd713b70612faee878cec56dceb9089f2e4ccf6
Suraj Kumar <140233201+Suraj1911kumar@users.noreply.github.com>
2024-03-14 17:35:57 +0530

    Add files via upload

---
+++ models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userModel = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+})
+
+const Users = mongoose.model('Users', userModel)
+
+
+module.exports = Users
---
+++ routes/userLogin.js
@@ -0,0 +1,34 @@
+const Users = require("../models/Users");
+const bycrpyt = require('bcrypt');
+
+const express = require('express')
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+    //console.log(req.body);
+    const { email, password } = req.body;
+
+
+    const user = await Users.findOne({ email: email })
+    const decrypt = await bycrpyt.compare(password, user.password)
+    try {
+        if (!user) {
+            return res.status(401).json({ msg: "No User Found" });
+        }
+        else {
+            if (decrypt === true) {
+                res.status(201).json({
+                    msg: 'login Successfull'
+                })
+            } else {
+                res.status(401).json({ msg: "Invalid Password!" });
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+
+})
+
+module.exports = router;
